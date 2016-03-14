154f7fd11102062b69c4ae3c230734da900e27b0
Hamish Willee <hamishwillee@gmail.com>
2016-03-15 09:42:12 +1100

    another minor change to update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ export PYTHONUNBUFFERED=1
 echo "$(date) starting"
 git fetch
 git pull origin master
-git reset --hard HEAD
+git reset --hard origin/master
 python update.py --clean True
 
 echo "$(date) done"
