c6968c01f750284e91b16d78ebe1d7e17b617ac9
Hamish Willee <hamishwillee@gmail.com>
2016-03-15 09:40:24 +1100

    minor change to update script

--- update.sh
+++ update.sh
@@ -5,11 +5,10 @@ export PYTHONUNBUFFERED=1
 (
 echo "$(date) starting"
 git fetch
+git pull origin master
 git reset --hard HEAD
 python update.py --clean True
 
 echo "$(date) done"
 ) >> build.log 2>&1
-
-#       echo "Failed planner common_pages for $dst" | mail -s "common pages $dst" andrew-3dr@tridgell.net,hamish@3drobotics.com
 
