26e8bbcc84f52841205a235e4f611f662bc2db37
Scott Slaughter <scott.slaughter@yahoo.com>
2017-02-10 23:07:09 -0800

    Clean up prints and comments, minor fixes

--- link_check.py
+++ link_check.py
@@ -9,16 +9,11 @@ cwd = os.getcwd()
 
 checked_directories = ["antennatracker", "common", "copter", "dev", "plane", "planner", "planner2", "rover"]
 #checked_directories = ["rover"]
-directories = []
 
-#Hardcoded directories for 
+directories = []
 for directory in checked_directories:
 	directories.append(cwd + "\\" + directory + "\source\docs\\")
 #Run through all directories and check all links in the document
-#https://mail.python.org/pipermail/tutor/2004-August/031232.html
-#http://stackoverflow.com/questions/8009882/how-to-read-large-file-line-by-line-in-python
-
-print directories
 
 outputfile = open('link_check_output.txt', 'w')
 
@@ -28,10 +23,8 @@ for src_dir in directories:
 	for file in dirlist:
 		if file != "supported-autopilot-controller-boards.rst": # different formatting in this file
 			with open(src_dir + file, 'r') as f:
-				outputfile.write("Directory: %s\n" % (file))
+				outputfile.write("Directory: %s\n" % (src_dir + file))
 				for line in f:
-					#outputfile.write("\tLine; %s\n" % (line))
-					#check for <> formatted links (most of them I think)
 					newline1 = line.split('<')
 					if len(newline1) > 1:
 						pos_link = newline1[1].split('>')
@@ -53,36 +46,28 @@ for src_dir in directories:
 									outputfile.write("\tLink: %s\n" % (pos_link[0]))
 									outputfile.write("\tLinkStatus: Bad, 404\n")
 									print "Bad Link"
-								elif status_code == 601:
+								elif status_code == 611:
 									outputfile.write("\tLink: %s\n" % (pos_link[0]))
-									outputfile.write("\tLinkStatus: Excpeted\n")
-									print "Bad Link"
+									outputfile.write("\tLinkStatus: Excepted\n")
+									print "Unknown Issue"
 
 								else:
 									pass
-									#outputfile.write("LinkStatus: Good\n")
-									#outputfile.write("Not 404, status: %d\n" % (status_code))
-									#print "Good Link"
 							else:
 								pass
-								print "not a website"
 						else:
 							pass
-							print "No good, moving on"
 					else:
 						pass
-						#print "No link, moving on."
-
-					#TODO check for non <> formatted links
-					#newline2 = line.split(':')
 		else:
 			pass
 
 		print "Done with file"
 
-
 outputfile.close()
 
-
+#Sources:
 #http://www.codepool.biz/python-check-broken-links-404.html
 #http://stackoverflow.com/questions/1140661/what-s-the-best-way-to-get-an-http-response-code-from-a-url#1140822
+#https://mail.python.org/pipermail/tutor/2004-August/031232.html
+#http://stackoverflow.com/questions/8009882/how-to-read-large-file-line-by-line-in-python
