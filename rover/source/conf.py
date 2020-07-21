fe1739bd56652f29624bb1845cd811a8bf9104b1
Stephen Dade <stephen_dade@hotmail.com>
2020-07-21 15:32:22 +1000

    Rover: Add user alerts

--- rover/source/conf.py
+++ rover/source/conf.py
@@ -164,6 +164,8 @@ html_favicon = '../../images/favicon_rover.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_js_files = ['./useralerts.js']
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
