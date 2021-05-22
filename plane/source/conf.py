230504e16c723ddd2f120d3a86b96d826a65fde2
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2021-05-22 20:15:33 +0100

    fix: Bumps year in confs to 2021
    Should resolve https://github.com/ArduPilot/ardupilot_wiki/issues/3555 if merged

--- plane/source/conf.py
+++ plane/source/conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Plane'
-copyright = u'2020, ArduPilot Dev Team'
+copyright = u'2021, ArduPilot Dev Team'
 author = u'ArduPilot Dev Team'
 
 # The version info for the project you're documenting, acts as replacement for
