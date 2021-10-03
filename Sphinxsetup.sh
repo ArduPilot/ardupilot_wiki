d21efecca2b0debfa1a8c9112625ec7b28e1d9f0
Henry Wurzburg <hwurzburg@yahoo.com>
2021-10-03 08:01:34 -0500

    Revert "Add python venv with specific sphinx version 1.8.5"
    This reverts commit 228b408c74637d99fcbb82e2e2ec06eb7aa33a50.

--- Sphinxsetup.sh
+++ Sphinxsetup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-SCRIPT_PATH=${0%/*}
-if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then 
-    cd $SCRIPT_PATH
-fi
-
 set -e
 set -x
 
@@ -22,7 +16,7 @@ if [ ${DISTRIBUTION_ID} = 'Ubuntu' ]; then
 fi
 
 sudo apt-get -y update
-sudo apt-get install -y unzip git imagemagick curl wget make python3 python3-venv
+sudo apt-get install -y unzip git imagemagick curl wget make python3
 
 # Install packages release specific
 if [ ${DISTRIBUTION_CODENAME} = 'bionic' ]; then
@@ -35,28 +29,24 @@ else
     fi
 fi
 
-# Create a fresh python venv for the ArduPilot wiki
-python3 -m venv --clear env/ardupilot_wiki_env
-source env/ardupilot_wiki_env/bin/activate
-
 # Get pip through the official website to get the lastest release
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm -f get-pip.py
 
 # Install sphinx
-python3 -m pip install --upgrade sphinx==1.8.5
+python3 -m pip install --user --upgrade sphinx
 
 # lxml for parameter parsing:
-python3 -m pip install --upgrade lxml
+python3 -m pip install --user --upgrade lxml
 
 # Install sphinx theme from ArduPilot repository
-python3 -m pip install --upgrade git+https://github.com/ArduPilot/sphinx_rtd_theme.git
+python3 -m pip install --user --upgrade git+https://github.com/ArduPilot/sphinx_rtd_theme.git
 
 # and a youtube plugin:
-python3 -m pip install --upgrade git+https://github.com/sphinx-contrib/youtube.git
+python3 -m pip install --user --upgrade git+https://github.com/sphinx-contrib/youtube.git
 
 # and a vimeo plugin:
-python3 -m pip install --upgrade git+https://github.com/ArduPilot/sphinxcontrib.vimeo.git
+python3 -m pip install --user --upgrade git+https://github.com/ArduPilot/sphinxcontrib.vimeo.git
 
 echo "Setup completed successfully!"
