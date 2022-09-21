6d025ed5cd2b4e6055ec1b83d8f1f1ca8d347efe
TunaLobster <charliepjohnson@gmail.com>
2022-09-21 14:27:34 -0500

    Move python packages to requirements.txt

--- Sphinxsetup.bat
+++ Sphinxsetup.bat
@@ -1,17 +1,8 @@
 rem remove any existing packages that may cause conflicts
 pip uninstall -y sphinx lxml sphinx-rtd-theme sphinxcontrib-youtube beautifulsoup4
 
-rem Install sphinx
-pip install --upgrade sphinx==7.1.2 "docutils<0.19" requests>=2.31.0 sphinx-tabs==3.*
-
-rem lxml for parameter parsing:
-pip install --upgrade lxml
-
-rem Install sphinx theme from ArduPilot repository
-pip install --upgrade git+https://github.com/ArduPilot/sphinx_rtd_theme.git
-
-rem and a youtube plugin:
-pip install --upgrade git+https://github.com/ArduPilot/sphinxcontrib-youtube.git
+rem Install required python packages
+pip install --user --upgrade -r requirements.txt
 
 echo "Setup completed successfully!"
 pause
--- Sphinxsetup.sh
+++ Sphinxsetup.sh
@@ -49,23 +49,8 @@ curl "$GET_PIP_URL" -o get-pip.py
 python3 get-pip.py
 rm -f get-pip.py
 
-# Install python packages using known working versions
-# Install sphinx with a specific docutils version
-python3 -m pip install --user --upgrade sphinx==${SPHINX_VERSION} "docutils<0.19"  "requests>=2.31.0" sphinx-tabs==3.*
-
-# lxml for parameter parsing:
-python3 -m pip install --user --upgrade lxml
-
-# Install sphinx theme from ArduPilot repository
-python3 -m pip install --user --upgrade git+https://github.com/ArduPilot/sphinx_rtd_theme.git
-
-# and youtube and video plugins:
-# This command might require a --force option if you have and older extension installed
-# Rerun Sphinxsetup.sh after doing that
-python3 -m pip install --user --upgrade git+https://github.com/ArduPilot/sphinxcontrib-youtube.git
-
-# Install flake8
-python3 -m pip install --user --upgrade flake8==3.7.9
+# Install required python packages
+python3 -m pip install --user --upgrade -r requirements.txt
 
 # Reset the value of DISPLAY
 if grep -qi -E '(Microsoft|WSL)' /proc/version; then
