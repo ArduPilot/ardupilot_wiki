67602e75c389d725b9574a04dd7ebf9827621994
Pierre Kancir <pierre.kancir.emn@gmail.com>
2023-06-13 12:26:35 +0200

    All: add plausible tracking

--- blimp/source/conf.py
+++ blimp/source/conf.py
@@ -153,7 +153,10 @@ html_favicon = '../../images/favicon_blimp.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_js_files = ['./useralerts.js']
+html_js_files = [
+    './useralerts.js',
+    ('https://plausible.ardupilot.org/js/script.outbound-links.js', {"data-domain": "ardupilot.org", "defer": "defer"}),
+    ]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
