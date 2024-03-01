df9790c7106fe9555cdd28de832e5622747983b1
TunaLobster <charliepjohnson@gmail.com>
2024-03-01 17:06:24 -0600

    Setup: Update for Ubuntu Mantic and EOL Lunar

--- Sphinxsetup.sh
+++ Sphinxsetup.sh
@@ -10,23 +10,23 @@ fi
 DISTRIBUTION_ID=$(lsb_release -i -s)
 if [ ${DISTRIBUTION_ID} = 'Ubuntu' ]; then
   DISTRIBUTION_CODENAME=$(lsb_release -c -s)
-  if [ ${DISTRIBUTION_CODENAME} = 'focal' ] || [ ${DISTRIBUTION_CODENAME} = 'bionic' ] || [ ${DISTRIBUTION_CODENAME} = 'lunar' ]; then
+  if [ ${DISTRIBUTION_CODENAME} = 'focal' ] || [ ${DISTRIBUTION_CODENAME} = 'bionic' ] || [ ${DISTRIBUTION_CODENAME} = 'mantic' ]; then
     sudo add-apt-repository universe -y
   fi
 fi
 
+sudo apt-get -y update
+sudo apt-get install -y unzip git imagemagick curl wget make python3
+
 # create a Python venv on more recent releases:
-if [ ${DISTRIBUTION_CODENAME} == 'lunar' ]; then
-    sudo apt install python3.11-venv
+if [ ${DISTRIBUTION_CODENAME} = 'mantic' ]; then
+    sudo apt install python3-venv
     python3 -m venv $HOME/venv-ardupilot-wiki --upgrade-deps
 
     # activate it:
     SOURCE_LINE="source $HOME/venv-ardupilot-wiki/bin/activate"
     $SOURCE_LINE
 fi
-
-sudo apt-get -y update
-sudo apt-get install -y unzip git imagemagick curl wget make python3
 
 # Install packages release specific
 if [ ${DISTRIBUTION_CODENAME} = 'bionic' ]; then
@@ -56,7 +56,7 @@ else
 fi
 
 PIP_USER_ARGUMENT=""
-if [ ${DISTRIBUTION_CODENAME} != 'lunar' ]; then
+if [ ${DISTRIBUTION_CODENAME} != 'mantic' ]; then
   curl "$GET_PIP_URL" -o get-pip.py
   python3 get-pip.py
   rm -f get-pip.py
