43f2eda38805a1fe077b0ed5d656f35f709f61e8
Peter Barker <pbarker@barker.dropbear.id.au>
2025-01-26 12:31:29 +1100

    initvagrant: set up build_wiki structure in /home/vagrant

--- scripts/initvagrant.sh
+++ scripts/initvagrant.sh
@@ -23,5 +23,35 @@ chown -R vagrant.vagrant "$OUTDIR"
 mkdir -p /var/sites/wiki-backup
 chown -R vagrant.vagrant /var/sites/wiki-backup
 
+cat <<"EOF" | sudo -H -u vagrant bash
+
+mkdir -p build_wiki
+
+pushd build_wiki
+mkdir -p logs
+
+# trees to partially clone
+for i in ardupilot_wiki sphinx_rtd_theme; do
+  if [ -e $i ]; then
+    pushd $i
+    git pull
+    popd
+  else
+    git clone https://github.com/ardupilot/$i --depth 1
+  fi
+done
+
+# trees to fully clone
+for i in ardupilot; do
+  if [ -e $i ]; then
+    pushd $i
+    git pull
+    popd
+  else
+    git clone https://github.com/ardupilot/$i
+  fi
+done
+
+EOF
 
 echo "---------- $0 end ----------"
