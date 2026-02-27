fa7351718064a3ce2557360ae216aec6efce0101
Pierre Kancir <pierre.kancir.emn@gmail.com>
2026-02-27 15:38:29 +0100

    common options: allow parallele read and write, cleanup warning

--- common_conf.py
+++ common_conf.py
@@ -2,6 +2,10 @@
 # This information is imported by the conf.py files in each of the sub wikis
 
 
+# Parallel reading of source files (use all available CPUs)
+parallel_read_safe = True
+parallel_write_safe = True
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -16,10 +20,6 @@ extensions = [
     'sphinx_tabs.tabs'        # For clickable tabs
 ]
 
-# Set False to re-enable warnings for non-local images.
-disable_non_local_image_warnings = True
-
-
 # wiki_base_url='https://dl.dropboxusercontent.com/u/3067678/share2/wiki'
 # intersphinx_base_url=wiki_base_url+'/%s/build/html/'
 
@@ -30,6 +30,11 @@ intersphinx_base_url = wiki_base_url + '%s/'
 # Where to point the base of the build for the main site menu
 html_context = {'target': '/'}
 # This needs to change to the actual URL root once the theme updated.
+
+# Don't generate search index for versioned parameter pages
+html_search_options = {
+    'dict_max_word_length': 40,  # Skip very long parameter names
+}
 
 # Known wiki keys (single source of truth)
 WIKI_KEYS = [
@@ -50,22 +55,15 @@ WIKI_KEYS = [
 intersphinx_mapping = {k: (intersphinx_base_url % k, None) for k in WIKI_KEYS}
 
 
-# PATCH REMOVE NON-LOCAL IMAGE WARNINGS
-# From:
-#  http://stackoverflow.com/questions/12772927/specifying-an-online-image-in-sphinx-restructuredtext-format
-#  And https://github.com/sphinx-doc/sphinx/issues/2429
+# Suppress warnings that slow down builds (already have nitpicky = False)
+suppress_warnings = [
+    'epub.unknown_project_files',  # Suppress epub warnings
+]
 
+disable_non_local_image_warnings = True
 
 if disable_non_local_image_warnings:
-    import sphinx.environment
-    from docutils.utils import get_source_line
-
-    def _warn_node(self, msg, node, **kwargs):
-        if not msg.startswith('nonlocal image URI found:'):
-            self._warnfunc(msg, '%s:%s' % get_source_line(node), **kwargs)
-
-    sphinx.environment.BuildEnvironment.warn_node = _warn_node
-# ENDPATH
+    suppress_warnings.append('image.nonlocal_uri')  # Suppress external image warnings
 
 
 def setup(app):
